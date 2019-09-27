5e93f904603d48ad7dd8b2b246d8edd881b6a037
Ramesh Ramalingam <rrama@thoughtworks.com>
2019-09-27 19:44:38 +0530

    TFS-148271 renamed import-map-overrides to min.js

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -1,4 +1,5 @@
 rm -rf ./dist && mkdir dist
 cp -r ./statics/ ./dist/
-cp -r ./node_modules/systemjs ./dist/systemjs@6.0.0
+cp -r ./node_modules/systemjs ./dist/systemjs@6.1.1
 cp -r ./node_modules/import-map-overrides ./dist/import-map-overrides@1.8.0
+mv ./dist/import-map-overrides@1.8.0/dist/import-map-overrides.js ./dist/import-map-overrides@1.8.0/dist/import-map-overrides.min.js
