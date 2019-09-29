8a7215cc3e26f48466a65f045f6093ff4f5809bc
Ramesh Ramalingam <rrama@thoughtworks.com>
2019-09-29 18:48:30 +0530

    TFS-148272 module-spa fixed to correct import-map-overrides file

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -2,4 +2,3 @@ rm -rf ./dist && mkdir dist
 cp -r ./statics/* ./dist/
 cp -r ./node_modules/systemjs ./dist/systemjs@6.1.1
 cp -r ./node_modules/import-map-overrides ./dist/import-map-overrides@1.8.0
-mv ./dist/import-map-overrides@1.8.0/dist/import-map-overrides.js ./dist/import-map-overrides@1.8.0/dist/import-map-overrides.min.js
