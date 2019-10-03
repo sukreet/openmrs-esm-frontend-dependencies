c11b76949387bfbcf653493f0abcfcd68e87afbd
Ramesh Ramalingam <rrama@thoughtworks.com>
2019-10-03 10:05:09 +0530

    Packmap changes

--- scripts/build-dist.sh
+++
@@ -1,4 +0,0 @@
-rm -rf ./dist && mkdir dist
-cp -r ./statics/* ./dist/
-cp -r ./node_modules/systemjs ./dist/systemjs@6.1.1
-cp -r ./node_modules/import-map-overrides ./dist/import-map-overrides@1.8.0
--- scripts/deploy-to-docker.sh
+++ scripts/deploy-to-docker.sh
@@ -1,3 +1,1 @@
-rm ./dist/import-map.json
-mv ./dist/import-map.local.json ./dist/import-map.json
-docker cp ./dist/. distro_web_1:/usr/local/tomcat/.OpenMRS/frontend/
+docker cp ./frontend/. distro_web_1:/usr/local/tomcat/.OpenMRS/frontend/
